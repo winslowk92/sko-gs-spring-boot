abd00662b086afffdca6b7dcb473fe1d99b1612a
Rene Cabral <nuzzio@users.noreply.github.com>
2020-01-30 16:39:22 -0500

    updated perl script to contain nexus

--- workshop/sample_code/GetSonarQubeDataFromJenkinsLog.pl
+++ workshop/sample_code/GetSonarQubeDataFromJenkinsLog.pl
@@ -1,66 +1,93 @@
-use strict; 
-use warnings; 
-  
-sub main 
-{ 
-	
-    my $jenkinsJobId = "$[/myPipelineRuntime/stages/validateRequirements/tasks/GetJenkinsJobLog/job/jobId]"; 
-    my $jenkinsJobName = "$[/myPipelineRuntime/stages/validateRequirements/tasks/GetJenkinsJobLog/job/jobName]"; 
+use strict;
+use warnings;
+
+sub main
+{
+
+    my $jenkinsJobId = "$[/myPipelineRuntime/stages/validateRequirements/tasks/GetJenkinsJobLog/job/jobId]";
+    my $jenkinsJobName = "$[/myPipelineRuntime/stages/validateRequirements/tasks/GetJenkinsJobLog/job/jobName]";
     my $sonarProjectVersion;
     my $sonarProjectKey;
     my $sonarProjectName;
+    my $nexusGroup;
+    my $nexusArtifact;
+    my $nexusVersion;
+
 	print "../$jenkinsJobName/\n";
 	# find the file log name
 	opendir(DIR, "../$jenkinsJobName/");
 	my @files = grep(/GetBuildLog.*\.log$/,readdir(DIR));
 	closedir(DIR);
-	
+
 	# check that we have one file
 	my $fileCount = scalar @files;
-	
+
 	if($fileCount != 1) {
 	 	die "There were $fileCount log files found\n";
 	}
 
     my $jenkinsLogFile = "../$jenkinsJobName/$files[0]";
-    
-    open(FH, $jenkinsLogFile) or die("File $jenkinsLogFile not found"); 
-      
-    while(my $String = <FH>) 
-    { 
-        if($String =~ /Uploaded:\s+http:\/\/.*(\d+\.\d+\.\d+(-SNAPSHOT)*?)\/([a-z\-]+)-(\d+\.\d+\.\d+-\d+\.\d+-\d+)\.jar/)  
-        { 
-            $sonarProjectVersion = "$1"; 
-        } 
-        # will match project key and each segment of the key
-        if($String =~ /\[INFO\]\s+Project\s+key:\s+(([a-z\-\d\.]+):([a-z\-\d]+))/) 
-        { 
-            $sonarProjectKey = "$1"; 
-            $sonarProjectName = "$2"; 
-        } 
-    } 
-    close(FH); 
-    
+
+    open(FH, $jenkinsLogFile) or die("File $jenkinsLogFile not found");
+
+    while(my $String = <FH>)
+    {
+
+        # From line: Uploaded: http://nexus.core-devops.com/repository/maven-releases/com/example/spring-boot/1.0.0/spring-boot-1.0.0.jar
+        # 1.0.0 or 1.0.0-SNAPSHOT as $1
+        if($String =~ /Uploaded:\s+http:\/\/.*(\d+\.\d+\.\d+(-SNAPSHOT)*?)\/([a-z\-]+)(-\d+\.\d+\.\d+)(-\d+\.\d+-\d+)?\.jar/)
+        {
+            $sonarProjectVersion = "$1";
+            $nexusVersion = "$1";
+        }
+
+        # From line: [INFO] Project key: com.example:my-project
+        # com.example:my-project as $1
+        # com.example as $2
+        # my-project as $3
+        if($String =~ /\[INFO\]\s+Project\s+key:\s+(([a-z\-\d\.]+):([a-z\-\d]+))/)
+        {
+            $sonarProjectKey = "$1";
+            $sonarProjectName = "$3";
+
+            $nexusGroup = "$2";
+            $nexusArtifact = "$3";
+        }
+
+    }
+    close(FH);
+
+
     print $sonarProjectVersion . "\n";
     print $sonarProjectKey . "\n";
     print $sonarProjectName . "\n";
-    
-    use ElectricCommander;
+
+
+	use ElectricCommander;
 	my $ec = ElectricCommander->new();
+
     $ec->createProperty({
-   				propertyName => "/myStageRuntime/sonarProjectVersion",
-          		value => $sonarProjectVersion
-    		});	
+   			propertyName => "/myStageRuntime/sonarProjectVersion",
+          	value => $sonarProjectVersion
+    	});
 	$ec->createProperty({
 			propertyName => "/myStageRuntime/sonarProjectKey",
 			value => $sonarProjectKey
-		});	
-
+		});
     $ec->createProperty({
-   				propertyName => "/myStageRuntime/sonarProjectName",
-          		value => $sonarProjectName
-    		});	
-    
-    
-} 
-main(); 
+   			propertyName => "/myStageRuntime/sonarProjectName",
+          	value => $sonarProjectName
+    	});
+    $ec->createProperty({
+   			propertyName => "/myStageRuntime/nexusGroup",
+          	value => $nexusGroup
+    	});
+	$ec->createProperty({
+			propertyName => "/myStageRuntime/nexusArtifact",
+			value => $nexusArtifact
+		});
+    $ec->createProperty({
+   			propertyName => "/myStageRuntime/nexusVersion",
+          	value => $nexusVersion
+    	});
+}
